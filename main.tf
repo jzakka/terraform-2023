f585587dcbf0c85e5a14bb0ca846120013d0362c
Jzakka <mouse4786@gmail.com>
2023-06-28 10:29:35 +0900

    security group

--- main.tf
+++ main.tf
@@ -1,20 +1,25 @@
 terraform {
+  // 이 부분은 terraform cloud에서 설정한 workspace의 이름과 동일해야 함
+  // 이 부분은 terraform login 후에 사용가능함
   cloud {
-    organization = "og-1-jzakka"
+    organization = "og-1"
 
     workspaces {
       name = "ws-1"
     }
   }
 
+  // 자바의 import 와 비슷함
+  // aws 라이브러리 불러옴
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = var.region
 }
@@ -28,9 +33,8 @@ resource "aws_vpc" "vpc_1" {
 }
 
 resource "aws_subnet" "subnet_1" {
-  vpc_id = aws_vpc.vpc_1.id
-
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.region}a"
 
   tags = {
@@ -59,7 +63,30 @@ resource "aws_route_table" "rt_1" {
   }
 }
 
+
 resource "aws_route_table_association" "association_1" {
-  subnet_id = aws_subnet.subnet_1.id
+  subnet_id      = aws_subnet.subnet_1.id
   route_table_id = aws_route_table.rt_1.id
+}
+
+resource "aws_security_group" "sg_1" {
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = aws_vpc.vpc_1.id
+
+  tags = {
+    Name = "${var.prefix}-sg-1"
+  }
 }

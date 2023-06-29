70519c6a853ece214b374d7b235d5adcf32e6e1b
Jzakka <mouse4786@gmail.com>
2023-06-29 11:14:03 +0900

    bucket

--- main.tf
+++ main.tf
@@ -145,3 +145,11 @@ resource "aws_instance" "ec2_1" {
     Name = "${var.prefix}-ec2-1"
   }
 }
+
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "${var.prefix}-jzakka-sample-1"
+
+  tags = {
+    Name = "${var.prefix}-jzakka-sample-1"
+  }
+}
